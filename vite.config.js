84e82d289d41cbeca13933fb8d4e09b6650c7be3
nicolashacku <nicolasjimenez089@gmail.com>
2025-12-08 22:32:25 -0500

    Show literal notes in weekly review

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/pareja-frontend/", // ⬅️ nombre del repo
+  base: "/pareja-frontend/",
 });
